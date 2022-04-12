8502adbb48e9e34b94c430c07c9ce674efd29811
zune <changzunelee@gmail.com>
2022-04-12 22:24:10 +0900

    java

---
+++ first/src/first/First.java
@@ -0,0 +1,25 @@
+package first;
+/**
+ * 별 두개 여기도 주석입니다. 다음 클래스에 나오게 할수 있다.
+ * <br>
+ * 이름을 출력하는 클랙스입니다.
+ */
+
+public class First {
+	
+	public static void main(String[] args) {
+		
+		
+		//슬러시 2개는 주석이다.
+		//부터 그줄의 끝까지 주석
+		/*주석의 주석 
+		 * 여기는 범위 주석입니다.
+		 */
+	
+		System.out.println("이창준입니다.");
+	
+		
+	}
+	
+
+}
---
+++ first/src/first/Test.java
@@ -0,0 +1,13 @@
+package first;
+
+public class Test {
+
+public static void main(String[] args) {
+	First p;
+	p = null;
+	System.out.println(p);
+	
+}
+	
+	
+}

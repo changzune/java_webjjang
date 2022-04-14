396480f6e302dde5dc8d3c85481d2aa9ad8f7425
zune <changzunelee@gmail.com>
2022-04-15 04:08:31 +0900

    java

--- ch06object/src/ch06object/Calculator.java
+++ ch06object/src/ch06object/Calculator.java
@@ -3,10 +3,23 @@ package ch06object;
 public class Calculator {
 	
 	// [접근제한자 + 기타 제한들] 리턴 타입 메서드명 ([전달변수들 = 파라메터들]){ 처리문들 }
-	// 전원을 켜는 처리메서드 
+	// 전원을 켜는 처리메서드(전원킨다.)
 	void powerOn() {
 		System.out.println("전원을 켭니다.");
 	}
+	
+	
+	// 더하기
+	int plus(int x, int y) {
+		return x + y;
+	}
+	// 나누기 
+	double divide(int x, int y) {
+		return (double)x / (double)y;
+	}
+	
+	
+	//전원을 끄는 처리메서드 (전원끈다.)
 	void powerOff() {
 		System.out.println("전원을 끕니다.");
 	}
---
+++ ch06object/src/ch06object/CalculatorExmaple.java
@@ -0,0 +1,28 @@
+package ch06object;
+
+public class CalculatorExmaple {
+	
+	public static void main(String[] args) {
+		// 클래스와 메서드를 만드는 목적 - 복잡하다. 반복된다.
+		// 계산기 가져와야 한다.new 한다.
+		Calculator myCalc = new Calculator();//생성자
+		// 계산기를 켠다. 
+		myCalc.powerOn();
+		// 더한다
+		int result1 =myCalc.plus(5, 6);
+		System.out.println(result1);
+		// 나눈다.
+		byte x = 10;
+		byte y = 4;
+		// 전달되는 값이 꼭 맞는 데이터를 전달하는 메서드를 
+		// 찾아간다. 그런데 딱맞는 데이터 타입없으면 자동캐스팅 되는곳을 찾아간다.
+		double result2 =myCalc.divide(10, 4);
+		System.out.println(result2);
+		// 전원을 끈다.
+		myCalc.powerOff();
+		
+		
+		
+	}
+
+}
